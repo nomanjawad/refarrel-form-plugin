fde0bc9cd57f278af3fa8f6daeef974bbcdc6de4
nomanjawad <nomanzawad@gmail.com>
2026-04-15 12:04:00 +0600

    Update GitHub repository URL in plugin definition

--- sds-referral-form.php
+++ sds-referral-form.php
@@ -18,7 +18,7 @@ define( 'SDS_VERSION', '1.0.1' );
 define( 'SDS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
 define( 'SDS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'SDS_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
-define( 'SDS_GITHUB_REPO', 'your-username/sds-referral-form' ); // Change to your GitHub repo
+define( 'SDS_GITHUB_REPO', 'https://github.com/nomanjawad/refarrel-form-plugin' ); // Change to your GitHub repo
 
 // Autoload classes
 require_once SDS_PLUGIN_DIR . 'includes/class-sds-activator.php';
